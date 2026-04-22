f00ac22742d1d61e22bdb3c4860d7b7f629050fe
MJ-CODER-TECH <mjcodertechagency@gmail.com>
2026-04-22 20:06:43 +0530

    Update API base URL for production environment

--- Admin/src/services/api.js
+++ Admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://admin-mjcoder.vercel.app/api',
+  baseURL: 'https://mj-coder-tech-agency.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
